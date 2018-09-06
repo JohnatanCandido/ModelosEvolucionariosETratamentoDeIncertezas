b1710498fba8c50fb9d6e094a65b1f66d27a4a60
Johnatan <john.acdc@hotmail.com>
2018-09-05 21:34:46 -0300

    Adicionada execução da função com x=1

--- programacao_genetica.py
+++ programacao_genetica.py
@@ -1,6 +1,7 @@
 from random import randint
 
 
+operadores = ['+', '-', '*', '/']
 valores = ['x', '+', '-', '*', '/', 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -24,19 +25,53 @@ def calc_prof(node, left=1, right=1):
     return left if left > right else right
 
 
-def printa_funcao(node):
+def printa_funcao(node, funcao=''):
     if node.left is not None:
-        printa_funcao(node.left)
+        funcao += printa_funcao(node.left, funcao)
     print(node.valor, end='')
+    funcao += str(node.valor)
     if node.right is not None:
-        printa_funcao(node.right)
+        funcao += printa_funcao(node.right)
+    return str(funcao)
+
+
+def executa_funcao(funcao, x):
+    while len(funcao) > 2:
+        i = 0
+        n1 = ''
+        n2 = ''
+        while funcao[i] not in operadores:
+            n1 += funcao[i] if funcao[i] != 'x' else x
+            i += 1
+        n1 = n1 if n1 != '' else 0
+        o = funcao[i]
+        i += 1
+        while i < len(funcao) and funcao[i] not in operadores:
+            n2 += funcao[i] if funcao[i] != 'x' else x
+            i += 1
+        r = executa_operacao(int(n1), o, int(n2))
+        funcao = str(r) + funcao[i:]
+    return x if funcao == 'x' else funcao
+
+
+def executa_operacao(n1, o, n2):
+    if o == '+':
+        return str(n1 + n2)
+    elif o == '-':
+        return str(n1 - n2)
+    elif o == '*':
+        return str(n1 * n2)
+    elif o == '/':
+        return str(int(n1 / n2))
 
 
 def gera_arvore():
     raiz = Node(valores[randint(0, 4)], None)
     print('Função: ', end='')
-    printa_funcao(raiz)
+    funcao = printa_funcao(raiz)
+    print(f'\nTeste:  {funcao}')
     print(f'\n\nProfundidade: {calc_prof(raiz)}')
+    print(f'Resultado: {executa_funcao(funcao, 1)}')
 
 
 if __name__ == '__main__':
