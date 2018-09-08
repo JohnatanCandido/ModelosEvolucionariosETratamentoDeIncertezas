b0e012a94451617ecd96b2e1e931e53a93c4fba6
Johnatan <john.acdc@hotmail.com>
2018-09-08 14:48:37 -0300

    criado método da roleta criado método de mutação criado gráfico para mostrar a diferença entre os resultados obtidos e esperados

--- programacao_genetica.py
+++ programacao_genetica.py
@@ -1,78 +1,269 @@
-from random import randint
+from copy import deepcopy
+from math import sqrt
+from random import randint, random
+import matplotlib.pyplot as plt
+import matplotlib.patches as mp
 
-
+MAX_INT = 99**9
+pares = [[1, 0.67], [2, 2], [3, 4], [4, 6.67], [5, 10], [6, 14], [7, 18.67], [8, 24], [9, 30], [10, 36.67]]
 operadores = ['+', '-', '*', '/']
-valores = ['x', '+', '-', '*', '/', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+valores = ['x', '+', '-', '*', '/', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+num_geracoes = 50
+tam_geracao = 250
+max_profundidade = 6
+chance_mutacao = 0.2
+
+
+class Geracao:
+    def __init__(self, populacao, geracao):
+        self.populacao = sorted(populacao, key=lambda x: x.fitness)
+        self.geracao = geracao
+        self.media_fitness = self.calc_media_fitness()
+        self.melhor = self.populacao[0]
+
+    def calc_media_fitness(self):
+        return self.calc_fitness_total() / len(self.populacao)
+
+    def calc_fitness_total(self):
+        fitness = 0
+        for ind in self.populacao:
+            fitness += ind.fitness
+        return fitness
+
+    def __str__(self):
+        return f'{self.geracao} ' \
+               f'- Média: {str(self.media_fitness)[:5] if self.media_fitness < MAX_INT else 999999} ' \
+               f'- Melhor: {str(self.melhor.fitness)[:6]} ' \
+               f'- Tamanho: {len(self.populacao)}'
 
 
 class Node:
-    def __init__(self, valor, raiz):
+    def __init__(self, valor, raiz, geracao=-1):
         self.valor = valor
-        if isinstance(valor, str) and valor != 'x':
+        self.raiz = raiz
+        if self.valor in operadores:
             self.left = Node(valores[randint(0, len(valores)-1)], self)
             self.right = Node(valores[randint(0, len(valores)-1)], self)
-        else:
-            self.left = None
-            self.right = None
+            if self.left.raiz != self or self.right.raiz != self:
+                raise AssertionError('Q merda velho')
+        if self.raiz is None:
+            self.geracao = geracao
+            self.funcao = cria_funcao(self)
+            self.profundidade = calc_prof(self)
+            self.lista = self.cria_lista_nos([])
+            if self in self.lista:
+                self.lista.remove(self)
+            self.fitness = -1
+            self.calc_fitness()
+            self.exp_vida = 5  # TODO Criar uma função de expectativa de vida
+
+    def refaz_raizes(self, raiz):
         self.raiz = raiz
+        if self.valor in operadores:
+            self.left.refaz_raizes(self)
+            self.right.refaz_raizes(self)
+
+    def cria_lista_nos(self, lista):
+        if self.raiz is not None or self.profundidade < max_profundidade:
+            try:
+                if self.valor in operadores:
+                    lista_esquerda = self.left.cria_lista_nos(lista)
+                    lista_direita = self.right.cria_lista_nos(lista)
+                    if lista_esquerda == -1 or lista_direita == -1:
+                        return -1
+                    lista += lista_esquerda
+                    lista += lista_direita
+                lista.append(self)
+                return lista
+            except MemoryError:
+                return -1
+        return []
+
+    def refresh(self):
+        self.refaz_raizes(None)
+        self.lista = self.cria_lista_nos([])
+        if self.lista != -1:
+            self.lista.remove(self)
+        self.funcao = cria_funcao(self)
+        self.calc_fitness()
+        return self
+
+    def calc_fitness(self):
+        fitness = 0
+        for par in pares:
+            try:
+                r = eval(self.funcao.replace('x', str(par[0])))
+            except ZeroDivisionError:
+                r = MAX_INT
+            fitness += (par[1] - r)**2
+        self.fitness = sqrt(fitness)
+        return self.fitness
+
+    def morreu(self, ger_atual):
+        return self.geracao + self.exp_vida < ger_atual
+
+
+def cria_populacao(ger):
+    populacao = []
+    for _ in range(tam_geracao):
+        raiz = Node(operadores[randint(0, 3)], None, ger)
+        if calc_prof(raiz) < max_profundidade and raiz.lista and raiz.fitness < MAX_INT:
+            populacao.append(raiz)
+    return populacao
+
+
+def gera_nova_geracao(geracao, num_ger):
+    # Dos que passam na roleta, tira aqueles cuja expectativa de vida + geração incial é menor que a geração atual
+    nova_populacao = [ind for ind in executa_roleta(geracao) if not ind.morreu(num_ger)]
+    return cruzar_populacao(nova_populacao, num_ger)
+
+
+def executa_roleta(geracao):
+    geracao.populacao = [ind for ind in geracao.populacao if ind.fitness < MAX_INT*0.8]
+    total = geracao.calc_fitness_total()
+    soma = 0
+    roleta = []
+    tam_desejado = len(geracao.populacao) * 0.75
+    if len(geracao.populacao) > tam_geracao * 10:
+        tam_desejado = len(geracao.populacao) * 0.10
+    nova_populacao = geracao.populacao[:]
+    for ind in nova_populacao:
+        roleta.append([ind, soma, ind.fitness / total + soma])
+        soma += ind.fitness / total
+    while len(nova_populacao) > tam_desejado:
+        numero = random()
+        for r in roleta:
+            if r[1] <= numero <= r[2]:
+                nova_populacao.remove(r[0])
+                roleta.remove(r)
+                break
+    return nova_populacao
+
+
+def cruzar_populacao(populacao, num_ger):
+    nao_cruzados = populacao[:]
+    max_cruzamento = len(nao_cruzados) * 0.20
+    while len(nao_cruzados) > max_cruzamento:
+        pai = nao_cruzados.pop(randint(0, len(nao_cruzados) - 1))
+        mae = nao_cruzados.pop(randint(0, len(nao_cruzados) - 1))
+
+        populacao += cruzar_individuos(pai, mae, num_ger)
+    print('')
+    return Geracao(populacao, num_ger)
+
+
+def cruzar_individuos(pai, mae, ger):
+    filho_1 = deepcopy(pai)
+    filho_2 = deepcopy(mae)
+
+    gene_1 = filho_1.lista[randint(0, len(filho_1.lista) - 1)]
+    gene_2 = filho_2.lista[randint(0, len(filho_2.lista) - 1)]
+
+    raiz_1 = gene_1.raiz
+    raiz_2 = gene_2.raiz
+
+    if raiz_2.left == gene_2:
+        gene_2.raiz.left = gene_1
+    elif raiz_2.right == gene_2:
+        gene_2.raiz.right = gene_1
+    else:
+        raise AssertionError('Isso não deveria ter acontecido')
+
+    if raiz_1.left == gene_1:
+        gene_1.raiz.left = gene_2
+    elif raiz_1.right == gene_1:
+        gene_1.raiz.right = gene_2
+    else:
+        raise AssertionError('Isso não deveria ter acontecido')
+
+    gene_1.raiz = raiz_2
+    gene_2.raiz = raiz_1
+
+    retorno = []
+    filho_1 = mutar(filho_1.refresh(), ger)
+    filho_2 = mutar(filho_2.refresh(), ger)
+    if calc_prof(filho_1) < max_profundidade and filho_1.lista and filho_1.fitness < MAX_INT:
+        retorno.append(filho_1)
+    if calc_prof(filho_2) < max_profundidade and filho_2.lista and filho_2.fitness < MAX_INT:
+        retorno.append(filho_2)
+
+    return retorno
+
+
+def mutar(ind, ger):
+    if random() < chance_mutacao:
+        print('.', end='')
+        remover = ind.lista[randint(0, len(ind.lista) - 1)]
+        if remover.raiz.left == remover:
+            remover.raiz.left = Node(valores[randint(0, len(valores)-1)], remover.raiz)
+        elif remover.raiz.right == remover:
+            remover.raiz.right = Node(valores[randint(0, len(valores) - 1)], remover.raiz)
+        else:
+            raise AssertionError('Isso não deveria ter acontecido')
+        del remover
+    ind.geracao = ger
+    ind.refresh()
+    return ind
 
 
 def calc_prof(node, left=1, right=1):
-    if node.left is not None:
+    if node.valor in operadores:
         left += calc_prof(node.left)
-    if node.right is not None:
         right += calc_prof(node.right)
     return left if left > right else right
 
 
-def printa_funcao(node, funcao=''):
-    if node.left is not None:
-        funcao += printa_funcao(node.left, funcao)
-    print(node.valor, end='')
+def cria_funcao(node, funcao=''):
+    if node.valor in operadores:
+        funcao += cria_funcao(node.left, funcao)
     funcao += str(node.valor)
-    if node.right is not None:
-        funcao += printa_funcao(node.right)
+    if node.valor in operadores:
+        funcao += cria_funcao(node.right)
+    if node.valor in['+', '-'] and node.raiz is not None and node.raiz.valor in['*', '/']:
+        funcao = '(' + funcao + ')'
     return str(funcao)
 
 
-def executa_funcao(funcao, x):
-    while len(funcao) > 2:
-        i = 0
-        n1 = ''
-        n2 = ''
-        while funcao[i] not in operadores:
-            n1 += funcao[i] if funcao[i] != 'x' else x
-            i += 1
-        n1 = n1 if n1 != '' else 0
-        o = funcao[i]
-        i += 1
-        while i < len(funcao) and funcao[i] not in operadores:
-            n2 += funcao[i] if funcao[i] != 'x' else x
-            i += 1
-        r = executa_operacao(int(n1), o, int(n2))
-        funcao = str(r) + funcao[i:]
-    return x if funcao == 'x' else funcao
+def printa_resultados(ind):
+    print('Num - Obtido - Esperado')
+    num = [par[0] for par in pares]
+    esperado = [par[1] for par in pares]
+    obtidos = []
+    for par in pares:
+        r = eval(ind.funcao.replace('x', str(par[0])))
+        print(f'{par[0]} - {str(r)[:5]} - {par[1]}')
+        obtidos.append(r)
+
+    plt.text(1, 30, 'Função: ' + ind.funcao)
+    plt.xlabel('Números')
+    plt.ylabel('Resultados')
+    plt.plot(num, obtidos, color='red')
+    plt.plot(num, esperado, color='blue')
+    red_patch = mp.Patch(color='red', label='Resultados Obtidos')
+    blue_patch = mp.Patch(color='blue', label='Resultados Esperados')
+    plt.legend(handles=[red_patch, blue_patch])
+    plt.show()
 
 
-def executa_operacao(n1, o, n2):
-    if o == '+':
-        return str(n1 + n2)
-    elif o == '-':
-        return str(n1 - n2)
-    elif o == '*':
-        return str(n1 * n2)
-    elif o == '/':
-        return str(int(n1 / n2))
+def main():
+    geracoes = []
+    for i in range(num_geracoes):
+        if not geracoes:
+            populacao = cria_populacao(i+1)
+            geracoes.append(Geracao(populacao, i+1))
+        else:
+            geracoes.append(gera_nova_geracao(geracoes[-1], i+1))
+        print(str(geracoes[-1]))
 
+    solucao = sorted(geracoes, key=lambda x: x.melhor.fitness)[0].melhor
 
-def gera_arvore():
-    raiz = Node(valores[randint(0, 4)], None)
-    print('Função: ', end='')
-    funcao = printa_funcao(raiz)
-    print(f'\nTeste:  {funcao}')
-    print(f'\n\nProfundidade: {calc_prof(raiz)}')
-    print(f'Resultado: {executa_funcao(funcao, 1)}')
+    print(f'\nFunção:  {solucao.funcao}')
+    print(f'Profundidade: {calc_prof(solucao)}')
+    print(f'Fitness: {solucao.fitness}')
+    print(f'Geração: {solucao.geracao}')
+    printa_resultados(solucao)
 
 
 if __name__ == '__main__':
-    gera_arvore()
+    main()
